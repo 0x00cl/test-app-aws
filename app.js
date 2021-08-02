ce0d2e5a7ce6b91bfb36a000cab75b6e7dd327c0
Tomas <tomas.gutierrez@solem.cl>
2021-08-02 18:13:54 -0400

    html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<!DOCTYPE html><html><head><title>Mis insectos</title></head><body style="background-color:green"><h1 style="color:red">Mi super sitio de insectos ta re loco</h1></body></html>');
 }).listen(process.env.PORT);
 
