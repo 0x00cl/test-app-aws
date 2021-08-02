475d2e11f717680b051c94670a701ce465307177
Tomas <tomas.gutierrez@solem.cl>
2021-08-02 18:12:56 -0400

    style

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n Env 2");
+  response.end('<!DOCTYPE html><html><head><title>Mis insectos</title></head><body style="background-color:green"><h1 style="color:red">Mi super sitio de insectos ta re loco</h1></body></html>');
 }).listen(process.env.PORT);
 
 console.log('App is running...');
