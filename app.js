13453580f64d52f2920581ac6f2d45706ddf2622
Tomas <tomas.gutierrez@solem.cl>
2021-08-02 18:43:41 -0400

    mosca

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<!DOCTYPE html><html><head><title>Mis insectos</title></head><body style="background-color:green"><h1 style="color:red">Mi super sitio de insectos ta re loco</h1></body></html>');
+  response.writeHead(404, {'Content-Type': 'text/html'});
+  response.end('<!DOCTYPE html><html><head><title>Mis insectos</title></head><body><h1>404</h1><p>El sitio no se encuentra disponible</p><p>Mientras tanto te dejo una mosca muerta</p><img src="/public/mosca.jpg"></body></html>');
 }).listen(process.env.PORT);
 
 console.log('App is running...');
